63216aa9b1191f09e6d5c440605b2a8eabe04441
MichaelDockers <dockers@outlook.com>
2022-05-12 18:07:44 +0300

    narcisstic func added

--- code_wars.py
+++ code_wars.py
@@ -1,7 +1,23 @@
 def spin_words(sentence: str) -> str:
-    reversed_sentence = [x[::-1] if len(x) > 3 else x for x in sentence.split()]
+    reversed_sentence = [x[::-1] if len(x) >= 5 else x for x in sentence.split()]
     return ' '.join(reversed_sentence)
 
 
-text = 'Hey teachers leave me alone'
-print(spin_words(text))
+def narcissistic(num: int) -> bool:
+    def base(num: int) -> int:
+        i = 0
+        while num > 0:
+            num //= 10
+            i += 1
+        return i
+
+    n = 0
+    st = base(num)
+    while num > 0:
+        n = n + (num % 10) ** st
+        num = num // 10
+    print(num, n)
+    return num == n
+
+
+print(narcissistic(1652))
