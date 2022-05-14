95619dd8738e45fcc4abafcfff7e3fc410cf63bf
dockers <dockers@yandex.ru>
2022-05-14 23:22:06 +0300

    fix

--- code_wars.py
+++ code_wars.py
@@ -1,6 +1,3 @@
-from re import L
-
-
 def spin_words(sentence: str) -> str:
     reversed_sentence = [x[::-1] if len(x) >= 5 else x for x in sentence.split()]
     return ' '.join(reversed_sentence)
